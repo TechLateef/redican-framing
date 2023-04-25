c48632735a62d775851d873800f06c1d412decd2
Xavier Fenton <xavierfenton99@gmail.com>
2023-04-26 09:14:50 +1200

    create home Component

---
+++ client/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Wallace Studio</h1> {/* Import an image*/}
+      {/* Nav component */}
+      {/* Social media component */}
+    </div>
+  )
+}
+
+export default Home
