f4a0cd51ac6bb14c3616b6ffa079600ed488ff0a
0qy <fordnicholasjones@gmail.com>
2023-04-26 13:30:54 +1200

    create an about component and include frontend route

---
+++ client/components/About.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function About() {
+    return(
+        <>
+        <h1>About:</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique laboriosam ratione explicabo cum aperiam dolor, fuga assumenda ipsam repudiandae nisi vero, illum autem dolore quo ullam atque sapiente qui!</p>
+        </>
+    )
+}
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Home from './Home'
 import Contact from './Contact'
+import About from './About'
 import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
