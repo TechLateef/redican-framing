dfcc4f26bdabc0a0194e9d0cc28804d4ba8446ac
0qy <fordnicholasjones@gmail.com>
2023-04-26 13:34:46 +1200

    create a link to contact page from about

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -1,10 +1,13 @@
 import React from "react"
+import { Link, Outlet } from "react-router-dom"
 
 export default function About() {
     return(
         <>
         <h1>About:</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique laboriosam ratione explicabo cum aperiam dolor, fuga assumenda ipsam repudiandae nisi vero, illum autem dolore quo ullam atque sapiente qui!</p>
+        <Link to="/contact">Contact</Link>
+        <Outlet />
         </>
     )
 }
