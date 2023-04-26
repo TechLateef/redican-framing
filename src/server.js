e3578ab0705bbbc04bd32bbf78b4d0078ecce0e6
0qy <fordnicholasjones@gmail.com>
2023-04-26 15:42:50 +1200

    create a get route for mongo shop data

--- src/server.js
+++ src/server.js
@@ -2,8 +2,13 @@ const path = require('path')
 const express = require('express')
 const server = express()
 
+const shopRoute = require('./routes/shopItems')
+
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
+
+server.use('/api/v1/shop', shopRoute)
+
 server.use('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './public/index.html'))
 })
