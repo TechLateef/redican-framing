859414e1c432595208cf99fa68a1109277084ded
0qy <fordnicholasjones@gmail.com>
2023-04-27 11:17:58 +1200

    create barebones shop component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from './Home'
 import Contact from './Contact'
 import About from './About'
+import Shop from './Shop'
 import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path='/store' element={<Shop />} />
       </Routes>
     </div>
   )
---
+++ client/components/Shop.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Nav from "./subcomponents/Nav"
+
+export default function Shop() {
+    return(
+        <>
+        <h1>shop</h1>
+        <img src="images/thumbnails/logo.jpg" alt="logo" className="logo"></img>
+        <Nav />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nemo magni natus esse soluta laboriosam libero odio illum, maxime vitae iure quibusdam, dolore eum delectus facere repellat. Similique, neque aliquid!</p>
+        </>
+    )
+}
