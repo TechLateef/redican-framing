ae34e507d62b24624ffcbc87da298eb284c34954
0qy <fordnicholasjones@gmail.com>
2023-04-27 16:51:04 +1200

    successfully pass data from parent as a prop

--- client/components/Shop.jsx
+++ client/components/Shop.jsx
@@ -1,10 +1,12 @@
 import React, {useState} from "react"
 import Nav from "./subcomponents/Nav"
+import ShopContainer from "./subcomponents/ShopContainer"
 import { fetchShopItems } from "../api/shopItems"
 
 export default function Shop() {
-    const [data, setData] = useState({})
+    const [data, setData] = useState([])
     const [loading, setLoading] = useState(true)
+
     window.onload = (e) => {
         e.preventDefault()
         setTimeout(() => {
@@ -16,18 +18,21 @@ export default function Shop() {
                 console.trace()
                 console.error(err)
             })
-            setLoading(false)
+        setLoading(false)
+            // if(data != []) {
+            //     setLoading(false)
+            // }
+
         }, 2000);
     }
-    console.log('component data: ', data)
+
     return(
         <>
         <h1>shop</h1>
         <Nav />
         {loading 
         ? <p>loading...</p>
-        : <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nemo magni natus esse soluta laboriosam libero odio illum, maxime vitae iure quibusdam, dolore eum delectus facere repellat. Similique, neque aliquid!</p>}
-        
+        : <ShopContainer data={data} />}
         </>
     )
 }
---
+++ client/components/subcomponents/ShopContainer.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function ShopContainer({data}) {
+    console.log('component data: ', data)
+    return(
+        <>
+        <p>test</p>
+        </>
+    )
+}
