b312ec098450294093d4e99d607ec443e7a1fb3a
0qy <fordnicholasjones@gmail.com>
2023-04-27 17:36:03 +1200

    render the mapped data conditionally

--- client/components/Shop.jsx
+++ client/components/Shop.jsx
@@ -18,10 +18,10 @@ export default function Shop() {
                 console.trace()
                 console.error(err)
             })
-        setLoading(false)
-            // if(data != []) {
-            //     setLoading(false)
-            // }
+        
+            if(data != []) {
+                setLoading(false)
+            }
 
         }, 2000);
     }
--- client/components/subcomponents/ShopContainer.jsx
+++ client/components/subcomponents/ShopContainer.jsx
@@ -1,10 +1,30 @@
 import React from "react"
 
 export default function ShopContainer({data}) {
-    console.log('component data: ', data)
+
+    const products = [...data]
+    
     return(
         <>
-        <p>test</p>
+        <div>
+        {products
+        ? products.map((product) => {
+            console.log(product.img_filename)
+            return(
+                <>
+                <div>
+                    <img src={`images/shopItems/${product.img_filename}`} alt="product_image" className="product_image"></img>
+                    <p>{product.title}</p>
+                </div>
+                </>)
+            })
+        :
+            <>
+                <h3>Oops! It looks like there we are having problems loading our stock.</h3>
+                <p>Refresh the page and try again, if the problem persists come check back in with us soon.</p>
+            </>
+            }
+        </div>
         </>
     )
 }
