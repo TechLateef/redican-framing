a2ed546f98243029c7d3c86d21b3f87fdfce7fb2
techlateef <techlateef@gmail.com>
2023-05-01 17:55:41 +0100

    added a route for deleting shop item from the database

--- src/db/shopItems.js
+++ src/db/shopItems.js
@@ -1,26 +1,44 @@
-const { MongoClient, ServerApiVersion } = require('mongodb')
-require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
-const usr = process.env.MONGODB_USERNAME
-const pw = process.env.MONGODB_PW
-const database = process.env.MONGODB_SHOP_DB
-const cltn = process.env.MONGODB_SHOP_CLN
-const urlCode = process.env.MONGODB_URL_CODE
+const usr = process.env.MONGODB_USERNAME;
+const pw = process.env.MONGODB_PW;
+const database = process.env.MONGODB_SHOP_DB;
+const cltn = process.env.MONGODB_SHOP_CLN;
+const urlCode = process.env.MONGODB_URL_CODE;
 
-const uri = `mongodb+srv://${usr}:${pw}@${urlCode}/?retryWrites=true&w=majority`
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+const uri = `mongodb+srv://${usr}:${pw}@${urlCode}/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function fetchShopItems() {
-       let dataArr = []
-      
-       const collection = client.db(database).collection(cltn).find()
-       await collection.forEach((data) => {
-           return dataArr.push(data)
-       })
+  let dataArr = [];
 
-   return dataArr
+  const collection = client.db(database).collection(cltn).find();
+  await collection.forEach((data) => {
+    return dataArr.push(data);
+  });
+
+  return dataArr;
+}
+
+/**
+ *
+ * @param {string} id -id of the item to delete
+ * @returns {object} the item deleted
+ */
+async function deleteshopItem(id) {
+  const item = client
+    .db(database)
+    .collection(cltn)
+    .findOneAndDelete({ _id: id });
+  return item;
 }
 
 module.exports = {
-   fetchShopItems,
-} 
+  fetchShopItems,
+  deleteshopItem,
+};
--- src/routes/shopItems.js
+++ src/routes/shopItems.js
@@ -1,16 +1,26 @@
-const express = require('express')
-const router = express.Router()
-const db = require('../db/shopItems')
+const express = require("express");
+const router = express.Router();
+const db = require("../db/shopItems");
 
-router.get('/', (req, res) => {
-    db.fetchShopItems()
+router.get("/", (req, res) => {
+  db.fetchShopItems()
     .then((data) => {
-        res.json({products: data.map((products) => products)})
+      res.json({ products: data.map((products) => products) });
     })
     .catch((err) => {
-        console.trace()
-        console.error(err)
-    })
-})
+      console.trace();
+      console.error(err);
+    });
+});
 
-module.exports = router
+//this route is for deleting a specific item in the database
+router.delete("/:itemId", async (req, res) => {
+  db.deleteshopItem(req.itemId)
+    .then((item) => {
+      res.json({ product: data });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+module.exports = router;
